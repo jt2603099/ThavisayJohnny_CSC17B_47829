d1fc59091c9bb069d07eb6ceb2c043dbd1892522
sonastea <sonastea@gmail.com>
2019-09-08 20:42:49 -0700

    Added hello world assignment

---
+++ Hmwk/Hello World - Setup Environment/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: jt2603099
+ *
+ * Created on September 8, 2019, 4:32 PM
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout << "Hello World." <<endl;
+    
+    
+    return 0;
+}
+
